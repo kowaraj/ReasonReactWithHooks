3d164dafdaf1e38c1f736f09a3fc968a40f102cb
kowaraj <kowaraj4stuff@gmail.com>
2019-05-05 17:37:53 +0200

    Add webpack-wev-middleware to the frontend

--- my-frontend/webpack.config.js
+++ my-frontend/webpack.config.js
@@ -5,22 +5,29 @@ const outputDir = path.join(__dirname, 'build/');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/Index.bs.js',
-  mode: isProd ? 'production' : 'development',
-  output: {
-    path: outputDir,
-    filename: 'Index.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: false
-    })
-  ],
-  devServer: {
-    compress: true,
-    contentBase: outputDir,
-    port: process.env.PORT || 8000,
-    historyApiFallback: true
-  }
+    entry: './src/Index.bs.js',
+    mode: isProd ? 'production' : 'development',
+
+    output: {
+	path: outputDir,
+	filename: 'Index.js',
+	publicPath: '/'
+
+    },
+
+    plugins: [
+	new HtmlWebpackPlugin({
+	    template: 'src/index.html',
+	    inject: false
+	})
+    ],
+
+    devServer: {
+	compress: true,
+	contentBase: outputDir,
+	port: 8000,
+	historyApiFallback: true
+    }
+
+    
 };
